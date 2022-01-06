259b3510a91671910bd30b397cf3c9e35ce08885
Aidan Gerber <agentaidan@gmail.com>
2022-01-06 10:55:04 -0600

    update adapter for cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import cloudflare from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: cloudflare(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
